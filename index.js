104295e0f1faed47a0add7e58c89103cf4043d26
Elijah Manor <elijah.manor@gmail.com>
2016-10-27 00:33:32 -0500

    Remove all the files

--- index.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node --harmony
-var path = require( "path" );
-var dir = path.basename( __dirname );
-
-require( "babel-register" )( {
-	ignore: false,
-	only: new RegExp( path.join( dir, "src" ) )
-} );
-require( "./src/index.js" );
--- spec/helpers.js
+++
@@ -1,7 +0,0 @@
-import { isObject } from "lodash";
-
-export function isPromise( promise ) {
-	return isObject( promise ) &&
-		promise.then instanceof Function &&
-		promise.catch instanceof Function;
-}
