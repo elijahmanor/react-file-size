7c335796bdf85ee54d31506c53b015815e5d5f21
Elijah Manor <elijah.manor@gmail.com>
2016-12-17 21:36:37 -0600

    Remove extra babel-register code

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 #!/usr/bin/env node --harmony
-const path = require( "path" );
-const dir = path.basename( __dirname );
 
-require( "babel-register" )( {
-	ignore: false,
-	only: new RegExp( path.join( dir, "src" ) )
-} );
+require( "babel-register" )();
 require( "./src/index.js" );
