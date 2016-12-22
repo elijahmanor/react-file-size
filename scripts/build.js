ee0e7284fa28357d5dfdff613d9cb8841a00560a
Elijah Manor <elijah.manor@gmail.com>
2016-12-21 21:50:30 -0600

    Use rimraf

--- scripts/build.js
+++ scripts/build.js
@@ -2,7 +2,7 @@ import { exec } from "shelljs";
 
 const version = process.env.npm_package_version;
 
-exec( `rm -rf public/${ version }` );
+exec( `rimraf public/${ version }` );
 exec( `pug --obj data.json src/index.pug --out public/${ version }/` );
 exec( `node-sass src/index.scss | postcss -c .postcssrc.json | cssmin > public/${ version }/index.min.css` );
 exec( `mustache data.json src/index.mustache.js | uglifyjs > public/${ version }/index.min.js` );
